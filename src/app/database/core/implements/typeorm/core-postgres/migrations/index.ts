5461ef20c6dc182921a1d9c76a7cac529392da21
Matheus Maidel <matheus.maidel@gmail.com>
2025-06-04 19:23:35 -0300

    feat: Initial migration.

--- src/app/database/core/implements/typeorm/core-postgres/migrations/index.ts
+++ src/app/database/core/implements/typeorm/core-postgres/migrations/index.ts
@@ -1,3 +1,6 @@
 import { MixedList } from 'typeorm';
+import { Init1749072458488 } from './1749072458488-init.migration';
 
-export const CORE_POSTGRES_MIGRATIONS: MixedList<Function | string> = [];
+export const CORE_POSTGRES_MIGRATIONS: MixedList<Function | string> = [
+  Init1749072458488,
+];
